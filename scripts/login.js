33a7916208f4ecfbeb6249c2c7d1b4e3ec1d4e79
Tiago Silva <tiagos3373@gmail.com>
2024-05-02 13:51:07 -0400

    style

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,6 @@
         const clientId = "d548203f7ceb4dd6a56164a2e97c3f17";
-        const redirectUri = "http://127.0.0.1:5500/project-milestone-2-geohits/index.html?"; // Replace with your own URL
+        const redirectUri = "http://127.0.0.1:5500/project-milestone-2-geohits/index.html?";// Replace with your own URL
+        // const redirectUri = "http://127.0.0.1:5501/index.html";
         // const redirectUri = new URL("./landing.html", window.location.href).href; // Needed for demo, since it may be hosted elsewhere
 
         // check for code returned
