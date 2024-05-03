50932299ff356b8f59b45a32dd4fa82a35ac4913
Tiago Silva <tiagos3373@gmail.com>
2024-05-03 14:55:41 -0400

    update

--- scripts/displayPlayList.js
+++ scripts/displayPlayList.js
@@ -25,5 +25,30 @@ for (let i = 0; i < songs.length; i++) {
         location.reload();
     })
 
+    songView.getElementById("moveUp").addEventListener("click", () => {
+        if (i != 0) {
+            let swap = songs[i - 1];
+            songs[i - 1] = songs[i];
+            songs[i] = swap;
+            localStorage.setItem("playList", JSON.stringify(songs));
+            location.reload();
+        } else {
+            alert("Cannot Move Song Up");
+        }
+    })
+
+    songView.getElementById("moveDown").addEventListener("click", () => {
+        if (i != (songs.length - 1)) {
+            let swap = songs[i];
+            songs[i] = songs[i + 1];
+            songs[i + 1] = swap;
+            localStorage.setItem("playList", JSON.stringify(songs));
+            location.reload();
+        } else {
+            alert("Cannot Move Song Down");
+        }
+    })
+
+
     songList.appendChild(songView);
 }
